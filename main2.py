e4ff7bbe9d58f55ebed3e2bc87bc4d9847cf47c8
MSE Ngoan Le <128952104+ngoan22mse23088@users.noreply.github.com>
2023-09-26 20:50:30 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -32,15 +32,15 @@ def setDevice1(state):
     else:
         ser.write(relay1_OFF)
 
-# while True:
-#     setDevice1(True)
-#     time.sleep(2)
-#     setDevice1(False)
-#     time.sleep(2)
-#     setDevice2(True)
-#     time.sleep(2)
-#     setDevice2(False)
-#     time.sleep(2)
+while True:
+    setDevice1(True)
+    time.sleep(2)
+    setDevice1(False)
+    time.sleep(2)
+    setDevice2(True)
+    time.sleep(2)
+    setDevice2(False)
+    time.sleep(2)
 
 
 import sys
